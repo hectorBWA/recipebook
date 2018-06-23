0f72e9960b7a5c1b82e5aac6355af9a68926c572
hectorBWA <hectorzavala12345@gmail.com>
2018-06-22 17:54:50 -0700

    managaing recipes in recipeService

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'recipes',
   templateUrl: './recipes.component.html', //external template
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
 
