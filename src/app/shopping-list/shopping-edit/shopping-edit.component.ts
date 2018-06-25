9f24f6dfea04b985961c94792a42c47eb10c65a0
hectorBWA <hectorzavala12345@gmail.com>
2018-06-25 10:31:55 -0700

    merge add-ingerdients into services

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../../shared/shopping-list.service';
 
@@ -12,15 +12,15 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
   addIngredient(){
-    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value, 
+      this.amountInputRef.nativeElement.value);
     
     this.shoppingListService.addIngredient(newIngredient);
   }
